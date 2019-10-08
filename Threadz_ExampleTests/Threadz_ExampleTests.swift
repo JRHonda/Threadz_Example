164ed9f8855551b27cc80eb5a65495649c1999ad
Justin Honda <justinohonda@gmail.com>
2019-10-08 19:07:32 -0400

    Initial Commit

---
+++ Threadz_ExampleTests/Threadz_ExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  Threadz_ExampleTests.swift
+//  Threadz_ExampleTests
+//
+//  Created by Justin Honda on 10/8/19.
+//  Copyright © 2019 Justin Honda. All rights reserved.
+//
+
+import XCTest
+@testable import Threadz_Example
+
+class Threadz_ExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
